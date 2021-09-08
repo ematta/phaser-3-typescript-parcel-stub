cb81a20b87cb830bc81362cc2f24e32ff21d4198
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2021-09-08 07:06:27 -0500

    chore: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 };
