4992e4d2a59da032c87bb64861809c600f084359
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2021-09-08 07:06:38 -0500

    feat: phaser stub

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import Phaser from "phaser";
+import GameScene from "./scenes/GameScene";
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: [GameScene],
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Phaser from "phaser";
+import config from "./config";
+
+const Game = new Phaser.Game(config);
+
+export default Game;
